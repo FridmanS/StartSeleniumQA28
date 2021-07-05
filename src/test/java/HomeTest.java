88268b4a485df31317057a8b56192fba8966ffd7
FridmanS <86373775+FridmanS@users.noreply.github.com>
2021-07-05 21:04:59 +0300

    Update HomeTest.java

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,7 +14,7 @@ public class HomeTest {
     WebDriver wd;
 
     @BeforeMethod
-    public void precondotion() {
+    public void preCondotion() {
         wd = new ChromeDriver();
         wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
     }
@@ -28,9 +29,73 @@ public class HomeTest {
         home.click();
     }
 
+    @Test
+    public void locatorTest(){
+        List<WebElement> list = wd.findElements(By.tagName("a"));
+        list.get(2).click();
+        //By.tagName
+        wd.findElement(By.tagName("a"));
+        wd.findElement(By.tagName("h1"));
+        wd.findElement(By.tagName("input"));
+        wd.findElement(By.tagName("button"));
+
+        wd.findElement(By.cssSelector("a"));
+        wd.findElement(By.cssSelector("button"));
+
+        //By.id
+        wd.findElement(By.id("root"));
+
+        wd.findElement(By.cssSelector("#root"));
+        wd.findElement(By.cssSelector("[id='root']"));
+
+        //By.className
+        wd.findElement(By.className("container"));
+        wd.findElement(By.className("active"));
+
+        wd.findElement(By.cssSelector(".container"));
+        wd.findElement(By.cssSelector("[class='active']"));
+
+        //By.name
+        //wd.findElement(By.name("namename"));
+        //wd.findElement(By.cssSelector("[name='namename']"));
+
+        //By.linkText
+        wd.findElement(By.linkText("HOME"));
+        wd.findElement(By.linkText("ABOUT"));
+        wd.findElement(By.partialLinkText("HO"));
+        wd.findElement(By.partialLinkText("AB"));
+
+        wd.findElement(By.cssSelector("[placeholder='Password']"));
+        //начинается ^=
+        wd.findElement(By.cssSelector("[placeholder^='Pass']"));
+        //заканчивается $=
+        wd.findElement(By.cssSelector("[placeholder$='word']"));
+        //содержит *=
+        wd.findElement(By.cssSelector("[placeholder*='sswo']"));
+    }
+
+    @Test
+    public void loginTest(){
+        wd.findElement(By.linkText("LOGIN")).click();
+        List<WebElement> elements = wd.findElements(By.tagName("input"));
+        WebElement email = elements.get(0);
+        WebElement password = elements.get(1);
+        email.click();
+        email.clear();
+        email.sendKeys("1234@mai.com" + Keys.TAB);
+
+        password.clear();
+        password.sendKeys("aA123456$");
+
+        List<WebElement> buttons = wd.findElements(By.tagName("button"));
+        buttons.get(0).click();
+
+
+    }
+
     @AfterMethod
     public void postCondotion(){
-        wd.quit();
+        //wd.quit();
     }
 }
 
